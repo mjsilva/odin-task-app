926db53a24fb268152e3e762112c9291e8f52001
Manuel Silva <manuel@mjs.me>
2023-01-16 17:09:48 +0700

    Fix typo on tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
   const textElement = screen.getByText(/React Todo App/i);
   expect(textElement).toBeInTheDocument();
